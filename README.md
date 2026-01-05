f06ad68e6519e226e4a42e77d505b5cf3ce3e3ad
Max Kung <maxkung06@gmail.com>
2026-01-05 04:00:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fortune Teller Pico
+
+## Requirements
+* 1 I2C LCD display
+* 1 PIR (Passive Infrared) motion sensor
+* 6 M to F Jumper wires
+* 1 Breadboard
+
+## About
+### Credits
+Programmed by Max Kung
---
+++ main.py
@@ -0,0 +1,50 @@
+from machine import I2C, Pin
+from time import sleep, sleep_ms
+from machine_i2c_lcd import I2cLcd
+import random
+
+i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=400000)
+addr = i2c.scan()[0]
+lcd = I2cLcd(i2c, addr, 2, 16)
+pir_sensor = Pin(15, Pin.IN)
+
+fortune1 = [
+    "You get good    ",
+    "You have a heart",
+    "Success is on   ",
+    "Your palm is an ",
+    "Playing well is ",
+    "Watch for birds "
+]
+
+fortune2 = [
+    "luck.",
+    "of gold.",
+    "your way.",
+    "A-OK.",
+    "winning.",
+    "coming soon."
+]
+def reset():
+    lcd.putstr("I am a          ")
+    lcd.putstr("Fortune Teller.")
+    sleep(1)
+
+def tellFortune():
+    lcd.clear()
+    x = random.randint(0, len(fortune1)-1)
+    lcd.putstr(fortune1[x])
+    lcd.putstr(fortune2[x])
+    sleep(6)
+    lcd.clear()
+
+reset()
+
+while True:
+    reading = pir_sensor.value()
+ 
+    if reading == 1:
+        tellFortune()
+        reset()
+    else:
+        pass
