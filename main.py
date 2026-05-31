7fa18f5cc5383f7e82ebeb2b29c948271c44731b
Max Kung <maxkung06@gmail.com>
2026-05-31 19:17:55 -0400

    Update main.py
    - Minor text fixes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from machine import I2C, Pin
-from time import sleep, sleep_ms
+from time import sleep
 from machine_i2c_lcd import I2cLcd
 import random
 
@@ -8,48 +8,45 @@ addr = i2c.scan()[0]
 lcd = I2cLcd(i2c, addr, 2, 16)
 pir_sensor = Pin(15, Pin.IN)
 
-# Write your descriptions here.
-
-# First line of the description
 fortune1 = [
-    "You get good    ",
-    "You have a heart",
-    "Success is on   ",
-    "Your palm is an ",
-    "Playing well is ",
-    "Watch for birds "
+	"You get good",
+	"You have a heart",
+	"Success is on",
+	"Your palm is an",
+	"Playing well is",
+	"Watch for birds"
 ]
 
-# Second line of the description
 fortune2 = [
-    "luck.",
-    "of gold.",
-    "your way.",
-    "A-OK.",
-    "winning.",
-    "coming soon."
+	"luck.",
+	"of gold.",
+	"your way.",
+	"A-OK.",
+	"winning.",
+	"coming soon."
 ]
-
 def reset():
-    lcd.putstr("I am a          ")
-    lcd.putstr("Fortune Teller.")
-    sleep(1)
+	lcd.putstr("I am a")
+	lcd.move_to(0, 1)
+	lcd.putstr("Fortune Teller.")
+	sleep(1)
 
 def tellFortune():
-    lcd.clear()
-    x = random.randint(0, len(fortune1)-1)
-    lcd.putstr(fortune1[x])
-    lcd.putstr(fortune2[x])
-    sleep(6)
-    lcd.clear()
+	lcd.clear()
+	x = random.randint(0, len(fortune1)-1)
+	lcd.putstr(fortune1[x])
+	lcd.move_to(0, 1)
+	lcd.putstr(fortune2[x])
+	sleep(6)
+	lcd.clear()
 
 reset()
 
 while True:
-    reading = pir_sensor.value()
+	reading = pir_sensor.value()
  
-    if reading == 1:
-        tellFortune()
-        reset()
-    else:
-        pass
+	if reading == 1:
+		tellFortune()
+		reset()
+	else:
+		pass
